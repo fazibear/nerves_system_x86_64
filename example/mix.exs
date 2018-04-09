58f22bf950ba30f460094a7ba205b5ea7c4201d6
Michał Kalbarczyk <fazibear@gmail.com>
2018-04-09 14:26:59 +0200

    Add nerves_shell

--- example/mix.exs
+++ example/mix.exs
@@ -60,7 +60,8 @@ defmodule Example.MixProject do
       {:shoehorn, "~> 0.2"},
       {:nerves_runtime, "~> 0.4"},
       {:nerves_network, "~> 0.3"},
-      {:nerves_firmware_ssh, "~> 0.2"}
+      {:nerves_firmware_ssh, "~> 0.2"},
+      {:nerves_runtime_shell, "~> 0.1.0"}
     ] ++ system(target)
   end
 
