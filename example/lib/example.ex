1c9e80a1555ef63ebf050951d21471140d22a744
Michał Kalbarczyk <fazibear@gmail.com>
2018-04-10 12:22:45 +0200

    Few thigs

--- example/lib/example.ex
+++ example/lib/example.ex
@@ -9,11 +9,22 @@ defmodule Example do
   def startx do
     spawn(fn -> MuonTrap.cmd("X", []) end)
     |> Process.register(Xorg)
+
     System.put_env("DISPLAY", ":0")
   end
 
   def xeyes do
-    spawn(fn -> MuonTrap.cmd("", []) end)
+    spawn(fn -> MuonTrap.cmd("xeyes", []) end)
     |> Process.register(Xeyes)
+  end
+
+  def xlog do
+    exec('cat /tmp/Xorg.0.log')
+  end
+
+  defp exec(cmd) do
+    cmd
+    |> :os.cmd
+    |> IO.puts
   end
 end
